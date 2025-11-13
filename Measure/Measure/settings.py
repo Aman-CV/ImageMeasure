f92dff68c322eb1d0caafdb60f66753e5839ad2f
notcamelcase01 <13annon@gmail.com>
2025-11-13 11:04:06 +0530

    generated new security key

--- Measure/Measure/settings.py
+++ Measure/Measure/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b@e$fsp%f+07n7j$a1upca^sz^-7!1)v2@^@eresjb!ys%=o5z'
-
+# SECRET_KEY = 'django-insecure-b@e$fsp%f+07n7j$a1upca^sz^-7!1)v2@^@eresjb!ys%=o5z'
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
