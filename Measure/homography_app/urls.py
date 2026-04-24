d67d83619b6300b7c69ee76508247beb0a2b6483
Aman-CV <aman.gautam@cadvisionengineers.com>
2026-04-24 12:03:42 +0530

    //

--- Measure/homography_app/urls.py
+++ Measure/homography_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -8,8 +8,8 @@ urlpatterns = [
     path('calibrate/',views.upload_calibration_video), #calibrate video and generate homo matrix
     #path('calibration_info/', views.get_homograph), #get calib info
     path('get_processed_video/', views.get_video_detail), #get processed video
-    path('stream_video/<int:video_id>/', views.video_stream, name='video_stream'),
-    #path('process_image/', views.process_image, name='process_image'),
+    re_path(r'^stream_video/(?P<video_id>\d+)/?$', views.video_stream, name='video_stream'),
+            #path('process_image/', views.process_image, name='process_image'),
     path('list_videos_by_assessment_and_test/', views.list_videos_by_assessment_and_test,
          name='list_videos_by_assessment_and_test'),
 ]
