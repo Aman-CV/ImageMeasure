99e97eb8eb477efd1a1194f961297b93615ce383
Aman-CV <aman.gautam@cadvisionengineers.com>
2026-04-28 08:50:57 +0530

    admin redirection

--- Measure/Measure/urls.py
+++ Measure/Measure/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('imagemeasure/admin', admin.site.urls),
     path('', include('homography_app.urls')),
+    
 ]
